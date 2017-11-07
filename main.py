d62a38186ec396228d9876355b493df9a10005a4
interface <627795061@qq.com>
2017-11-07 20:48:06 +0800

    add spider 2017年 11月 07日 星期二 20:48:05 CST

--- main.py
+++ main.py
@@ -7,4 +7,7 @@
  # @date:		2017-11-07
 ##]]
 
+from dao import Dao
+from spider import Spider
+
 if __name__ == '__main__':
--- spider.py
+++ spider.py
@@ -6,5 +6,39 @@
  # @email:	  	627795061@qq.com
  # @date:		2017-11-07
 ##]]
+import urllib  
+import urllib2
+
+class Spider():
+
+	def __init__(self):
+		self.user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+		self.header = { 'User-Agent' : user_agent }
+
+	def POST(self, url, header = None, data = {}):
+		header = header or self.header
+		try:
+			data = urllib.urlencode(values)
+			request = urllib2.Request(url, data, header)
+			response = urllib2.urlopen(request, timeout = 10)
+			page = response.read()
+			return page
+		except urllib2.URLError, e:
+			print e.reason
+			return None
+
+	def GET(self, url, header = None):
+		header = header or self.header
+		try:
+			data = urllib.urlencode(values)
+			request = urllib2.Request(url,header = header)
+			response = urllib2.urlopen(request, timeout = 10)
+			page = response.read()
+			return page
+		except urllib2.URLError, e:
+			print e.reason
+			return None
+
+
 
 if __name__ == '__main__':
